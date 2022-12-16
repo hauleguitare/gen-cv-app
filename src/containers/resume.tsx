aca4e0ee42a6d7df38fba204ba42ebdf4309b08b
hauleguitare <alexander.dutyone@gmail.com>
2022-12-17 00:50:23 +0700

    fix: fix style

--- src/containers/resume.tsx
+++ src/containers/resume.tsx
@@ -5,6 +5,7 @@ import Sidebar from "../components/sidebar";
 import useAppProvider from "../context/appContext";
 import createAppService from "../services/app.service";
 import { CvData, CvDataResp, CVJSON } from "../types/data.model";
+import "./styles.css";
 
 interface IGenCvComponentProps {}
 
@@ -15,18 +16,6 @@ const ResumeComponent: React.FunctionComponent<IGenCvComponentProps> = (props) =
   if (!dataLoader) {
     return null;
   }
-  //   React.useEffect(() => {
-  //     async function getData() {
-  //       const data = await appService.get<CvDataResp>("GetResume", "byid", "1217");
-  //       if (data.length <= 0) {
-  //         return;
-  //       }
-  //       const convertObject = appService.convertObject<CVJSON>(data[0].cvjson);
-  //       data[0].cvjson = convertObject;
-  //       setData(data[0]);
-  //     }
-  //     getData();
-  //   }, []);
 
   const onPrint = () => {
     window.print();
@@ -35,7 +24,7 @@ const ResumeComponent: React.FunctionComponent<IGenCvComponentProps> = (props) =
   return (
     <React.Fragment>
       <div className="w-full flex justify-center">
-        <button onClick={onPrint} className="py-2 px-2 bg-blue-500 rounded-md mt-4">
+        <button onClick={onPrint} className="py-2 px-2 bg-blue-500 rounded-md mt-4 noprint">
           PRINT PDF
         </button>
       </div>
